5fbf6f51501ec724299ef95585e039b8361e95ec
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-30 19:15:34 +0200

    init

--- AnimatingStrokeAndStrokeBorderTheDistinction/ContentView.swift
+++ AnimatingStrokeAndStrokeBorderTheDistinction/ContentView.swift
@@ -9,8 +9,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showStroke: Bool = false
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            RadialGradient(gradient: Gradient(colors: [Color.yellow, Color.red]), center: .center, startRadius: 50, endRadius: 500)
+                .edgesIgnoringSafeArea(.all)
+            
+            Capsule()
+                .stroke(lineWidth: showStroke ? 1 : 40)
+                .frame(width: 180, height: 280)
+                .foregroundColor(.orange)
+                .animation(Animation.interpolatingSpring(stiffness: 60, damping: 15).delay(1))
+                .shadow(color: .orange, radius: 10, x: 0, y: 0)
+            
+            Circle()
+                .frame(width: 70, height: 70)
+                .foregroundColor(.orange)
+                .shadow(color: .orange, radius: 20, x: 0, y: 0)
+                .scaleEffect(showStroke ? 1 : 1.4, anchor: .center)
+                .animation(Animation.interpolatingSpring(stiffness: 60, damping: 10).delay(0.8))
+                
+            Button(action: {
+                self.showStroke.toggle()
+            }) {
+                Image(systemName: "xmark")
+                    .font(.largeTitle)
+            }
+            .foregroundColor(.white)
+        }
     }
 }
 
